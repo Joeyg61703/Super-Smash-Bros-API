383420fa473c3aa5a4cd787a44221c6867646ba2
Joeyg61703 <Joeyg61703@gmail.com>
2022-06-07 17:32:16 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.MONGO_URL)
+.then(client => {
+
+    db = client.db('smash');
+    fighterCollection = db.collection('fighters');
+    console.log('Connected to Database');
+    app.set('view engine', "ejs");
+    app.set('views', 'views');
+    
+    app.get('/', (req,res)=>{
+        res.render("index.ejs")
+    })
+    app.get('/api/', (req,res)=>{
+        fighterCollection.find().toArray().then(result => {
+            res.send(result)
+        }).catch(err => console.error(err));
+    })
+    app.get('/api/:fighter', (req,res)=>{    
+        const fighterName = req.params.fighter;
+        const name = fighterName.split(' ').map(e =>  e[0].toUpperCase() + e.substring(1).toLowerCase()).join(' ');
+        fighterCollection.find({name: name}).toArray().then(result => {
+            const fighter = result[0];
+           res.send(fighter)
+        }).catch(err => console.error(err));
+    })
+    app.get('/:fighter', (req,res)=>{
+        const fighterName = req.params.fighter;
+        const name = fighterName.split(' ').map(e =>  e[0].toUpperCase() + e.substring(1).toLowerCase()).join(' ');
+        fighterCollection.find({name: name}).toArray().then(result => {
+            const fighter = result[0];
+            if(fighter){
+                res.render("fighter.ejs", {
+                    obj: fighter,
+                    name: fighter.name,
+                    game: fighter.game,
+                    jumpCount: fighter.jumpCount,
+                    weight: fighter.weight,
+                    img: fighter.img
+                })
+            }
+           else{
+               res.send("Not a fighter Idiot")
+           }
+        }).catch(err => console.error(err));
+    })
+    
+    app.listen(PORT, ()=> console.log(`Server Started on Port: ${PORT}`));
+})
+.catch(err => console.error(err));
